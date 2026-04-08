414e88a15fabc9e7f9281d1b3d00f6e3ee0d1247
Abin Joshua Hermon <abinjoshua3@gmail.com>
2026-04-08 17:55:05 +0530

    Add files via upload
    [UPDT]08/04/2026

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Hide Menus for Users",
+    'version': "1.0",
+    'licence': "LGPL-3",
+    'author': "Cybrosys",
+    'website': "http://www.cybrosys.com",
+    'sequence': 1,
+    'application': True,
+    'depends': ['base','purchase','contacts'],
+    'auto_install': True,
+    'data': [
+        'views/average_cost_views.xml',
+    ]
+}
